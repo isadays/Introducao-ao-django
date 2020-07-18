de7584e7ed772f6f59206ca61c50a9c62879b68e
isadays <isah.pld@gmail.com>
2020-07-18 13:04:44 -0300

    Commit first app -

--- core/views.py
+++ core/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render, HttpResponse
 
+
 # Create your views here.
-def hello(request,nome,idade):
-    return HttpResponse('<h1>Meu primeiro Projeto Django. Olá {}, de {} anos !</h1>'.format(nome,idade))
+def hello(request, nome, idade):
+    return HttpResponse('<h1>Meu primeiro Projeto Django. Olá {}, de {} anos !</h1>'.format(nome, idade))
+
+
+def soma(request, num1, num2):
+        return HttpResponse('A soma entre {} e {} é igual a {}'.format(num1, num2, num1 + num2))
+
+
+def divisao(request,num1,num2):
+        return HttpResponse('A divisao entre {} e {} é igual a {}'.format(num1, num2, num1 / num2))
+
+
+def subtracao(request,num1,num2):
+       return HttpResponse('A subtracao entre {} e {} é igual a {}'.format(num1, num2, num1 - num2))
+
+
+def multiplicacao(request,num1,num2):
+    return HttpResponse('A multiplicacao entre {} e {} é igual a {}'.format(num1, num2, num1 * num2))
